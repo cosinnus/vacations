2c19d1a8b6cd60edc747716ec18b40b6674611c9
cosinnus <51507290+cosinnus@users.noreply.github.com>
2021-04-17 10:42:35 +0700

    update eloquent SoftDeletes

--- app/TravelPackage.php
+++ app/TravelPackage.php
@@ -3,7 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\ELoquent\SoftDeletes;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class TravelPackage extends Model
 {
